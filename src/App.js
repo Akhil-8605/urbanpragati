dea2713a479363c93d17349f6903dd8f54c92958
Akhil-8605 <akhileshadam186@gmail.com>
2026-03-11 14:18:55 +0530

    github pages deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LoginPage from './Auth/login/LoginPage';
 import Signup from './Auth/signup/Signup';
